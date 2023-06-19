1c66e7462175b464f7a0465a34884ce2967732c1
helloworld442 <69632137+helloworld442@users.noreply.github.com>
2023-06-19 09:36:24 +0900

    [level 1] Title: 소수 만들기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,21 @@
+function prime(num) {
+    for(i = 2; i <= Math.sqrt(num);i++) {
+        if(num % i === 0) return false
+    }
+    return true
+}
+
+function solution(nums) {
+    let answer = 0;
+    for(let i = 0; i < nums.length;i++) {
+        for(let j = i+1;j < nums.length;j++) {
+            for(let k = j+1;k<nums.length;k++) {
+                sum = nums[i] + nums[j] + nums[k]
+                if(prime(sum)) {
+                    answer++;
+                }
+            }
+        }
+    }
+    return answer
+}
