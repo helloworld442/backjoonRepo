1bafb5b16cd38f1604ef2f77cccf51248d23130a
kim min seung <69632137+helloworld442@users.noreply.github.com>
2024-11-29 08:39:00 +0900

    [Silver III] Title: N과 M (4), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,21 @@
+N , M = map(int , input().split(" "))
+
+nums = [n for n in range(1 , N + 1)]
+
+depth = 0
+perm = []
+
+def permutation(start , depth):
+    if depth == M:
+        print(' '.join(perm))
+        return
+    
+    for i in range(start , N):
+        perm.append(str(nums[i]))
+        permutation(i, depth + 1)
+        perm.pop()
+
+permutation(0, depth)
+
+
+
