a04099fc7188991308528dce5eeea6d00a64d289
kim min seung <69632137+helloworld442@users.noreply.github.com>
2024-12-02 10:31:47 +0900

    [Bronze V] Title: X보다 작은 수, Time: 40 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -0,0 +1,12 @@
+N , X = map(int , input().split(' '))
+
+A = list(map(int , input().split(' ')))
+
+answer = []
+
+for a in A:
+    if a < X:
+        answer.append(str(a))
+
+print(' '.join(answer))
+         
